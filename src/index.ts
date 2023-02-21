d7ea3b3896483d0113da1e2d786f4b20cb89809a
Marc H. Weiner <mhweiner234@gmail.com>
2023-02-21 14:57:24 -0500

    fix: fix request duration
    - Fixes a bug where the request duration in ms was not correct, since we were missing the Request.on('finish') event code block
    
    BREAKING CHANGES: changed the info message and parameters slightly

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,23 @@
 import {NextFunction, Request, Response} from 'express';
 import {logger} from 'jsout';
-
-function getActualRequestDurationInMilliseconds(start: [number, number]) {
-
-    const NS_PER_SEC = 1e9; // convert to nanoseconds
-    const NS_TO_MS = 1e6; // convert to milliseconds
-    const diff = process.hrtime(start);
-
-    return ((diff[0] * NS_PER_SEC) + diff[1]) / NS_TO_MS;
-
-}
+import * as process from 'process';
 
 export function logRequest(req: Request, res: Response, next: NextFunction) {
 
-    const method = req.method;
-    const url = req.url;
-    const status = res.statusCode;
     const start = process.hrtime();
-    const durationMs = getActualRequestDurationInMilliseconds(start);
 
-    logger.info('REQ', {
-        method,
-        url,
-        status,
-        durationMs,
+    res.on('finish', () => {
+
+        const {method, url, hostname, ip, statusCode, statusMessage} = req;
+        const diff = process.hrtime(start);
+        const ms = ((diff[0] * 1e9) + diff[1]) / 1e6;
+
+        logger.info('req', {
+            method, url, hostname, ip, statusCode, statusMessage, ms,
+        });
+
     });
+
     next();
 
 }
